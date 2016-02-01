0bf4c4129b9a97c9b0f3e91b2a12610d2ceac19b
James S. Parker <jame-sparker@users.noreply.github.com>
2016-02-01 15:14:14 +0900

    Update places.spec.js
    Added tests for nearest and withinRectangle. Both are passing

--- test/places.spec.js
+++ test/places.spec.js
@@ -13,9 +13,34 @@ describe("Geocore User API", function() {
         .list(new geocore.utils.CommonOptions().setNum(5))
         .should.eventually.have.length(5);
     });
+
+    it("query based method should return a list of 5 places", function() {
+      var newQuery = new geocore.places.query();
+      return newQuery.setNum(5).all().should.eventually.have.length(5);
+      
+    });
+
+    it("Nearest function should not return an error", function() {
+      var newQuery = new geocore.places.query();
+      return newQuery
+              .setNum(10)
+              .setCenter(35.671018, 139.724336)
+              .nearest()
+              .should.eventually.have.length(10);
+
+    });
+
+    it("WithinRectangle function should not return an error", function() {
+      var newQuery = new geocore.places.query();
+      return newQuery
+              .setNum(10)
+              .setRectangle(35.6233362620001, 35.6835510000001,
+                            139.7086145, 139.782342333)
+              .withinRectangle()
+              .should.eventually.have.length(10);
+
+    });
+
+
   });
-
-
-
-
 });
